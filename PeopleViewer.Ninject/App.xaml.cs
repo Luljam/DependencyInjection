616fbcc9c8de149e39f02a8522fdecbed8d5772f
Luciano Frutuoso de Brito <britojam@hotmail.com>
2022-03-16 20:41:30 -0300

    Using Ninject

---
+++ PeopleViewer.Ninject/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Ninject;
+using PeopleViewer.Common;
+using PersonDataReader.Service;
+using System.Windows;
+
+namespace PeopleViewer.Ninject
+{
+    public partial class App : Application
+    {
+        IKernel Container = new StandardKernel();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Application.Current.MainWindow.Title = "With Dependency Injection - Ninject";
+            Application.Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            Container.Bind<IPersonReader>().To<ServiceReader>();
+        }
+
+        private void ComposeObjects()
+        {
+            Application.Current.MainWindow = Container.Get<PeopleViewerWindow>();
+        }
+    }
+}
